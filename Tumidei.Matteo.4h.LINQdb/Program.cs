d9e132bf7c6c726b960be1b797ab2b72a699725a
Matteo Tumidei <116791499+Tuminho1@users.noreply.github.com>
2023-05-09 20:36:11 +0200

    Add files via upload

---
+++ Tumidei.Matteo.4h.LINQdb/Program.cs
@@ -0,0 +1,23 @@
+﻿using SQLite;
+
+
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+
+
+var tblArtists = cn1.Query<Artist>("select * from artists");
+
+Console.WriteLine($"In questa tabella ci sono {tblArtists.Count} record!");
+
+
+
+tblArtists = tblArtists.OrderByDescending(x=>x.Name).ToList();
+foreach(var artista in tblArtists)
+{
+    Console.WriteLine($"{artista.Name}");
+}
+
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
